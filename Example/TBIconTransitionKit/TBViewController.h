59e771b9318b5753ff17556d74ca5b26c1ea1dae
AlexeyBelezeko <alexey.belezeko@sfcd.com>
2015-07-15 01:13:50 +0300

    Initial commit

---
+++ Example/TBIconTransitionKit/TBViewController.h
@@ -0,0 +1,13 @@
+//
+//  TBViewController.h
+//  TBIconTransitionKit
+//
+//  Created by AlexeyBelezeko on 07/15/2015.
+//  Copyright (c) 2015 AlexeyBelezeko. All rights reserved.
+//
+
+@import UIKit;
+
+@interface TBViewController : UIViewController
+
+@end
---
+++ Example/TBIconTransitionKit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TBIconTransitionKit
+//
+//  Created by AlexeyBelezeko on 07/15/2015.
+//  Copyright (c) 2015 AlexeyBelezeko. All rights reserved.
+//
+
+@import UIKit;
+#import "TBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TBAppDelegate class]));
+    }
+}
